68f3cf89f6c3b34c9931193bbcd2f4a2e1b3c078
Shadov <shadov.profile@gmail.com>
2019-02-10 21:08:19 +0100

    Connected to boot-admin-server (listing all manage endpoints as workaround to being unable to exclude only health from keycloak configuration)

--- core/api/src/main/kotlin/com/shadov/codehellven/api/code/CodeController.kt
+++ core/api/src/main/kotlin/com/shadov/codehellven/api/code/CodeController.kt
@@ -2,16 +2,23 @@ package com.shadov.codehellven.api.code
 
 import com.shadov.codehellven.api.code.domain.CodeApi
 import com.shadov.codehellven.api.code.entity.CodeRequestEntity
+import com.shadov.codehellven.common.lazyLogger
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @CrossOrigin
-@RequestMapping("/code")
+@RequestMapping("/api/code")
 internal class CodeController(private val codeApi: CodeApi) {
+    private val log by lazyLogger()
 
     @PostMapping("/execute")
     fun execute(@RequestBody request: CodeRequestEntity): ResponseEntity<String> {
         return ResponseEntity.accepted().body(codeApi.execute(request).getOrElseThrow { ex -> ex })
+    }
+
+    @GetMapping("/get")
+    fun get(): ResponseEntity<String> {
+        return ResponseEntity.accepted().body("whatever")
     }
 }
